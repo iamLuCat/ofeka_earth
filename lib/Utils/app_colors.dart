5b1d383a6d837afe5e2fc1213db4d3a11d2cdcaf
Martino Yovo <martinosoyen@gmail.com>
2024-04-20 15:06:51 -0400

    Update dialogs and add life status icon

--- lib/Utils/app_colors.dart
+++ lib/Utils/app_colors.dart
@@ -3,4 +3,5 @@ import 'dart:ui';
 class AppColors {
   static const redColor = Color.fromARGB(255, 123,6,10);
   static const yellowColor = Color.fromARGB(255, 255, 192, 17);
+  static const greenColor = Color(0xFF40E84E);
 }
